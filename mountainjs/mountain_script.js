93c325997724e9f379e946f033ba7c6274d29c7e
tyermercado <leinster.tyer12@gmail.com>
2023-04-01 11:46:32 +0800

    first commit

---
+++ mountainjs/mountain_script.js
@@ -0,0 +1,37 @@
+import { displayCartItems } from './mountain_cart.js';
+import { displayData } from './mountain_products.js';
+
+const iconQtyAction = document.querySelector('.header__icon');
+
+//fetch all data
+function fetchData() {
+  fetch('../mountainbike.json')
+    .then((data) => data.json())
+    .then((response) => displayData(response))
+    .catch((err) => console.error(err.message));
+}
+
+function getLocalStorage(description) {
+  return localStorage.getItem(description)
+    ? JSON.parse(localStorage.getItem(description))
+    : undefined;
+}
+
+function setLocalStorage(description, article) {
+  localStorage.setItem(description, JSON.stringify(article));
+  if (description === 'currentCart') displayCartItems(article);
+}
+
+iconQtyAction.addEventListener('click', handlePageView);
+
+function handlePageView() {
+  const allProductsApp = document.querySelector('.allProductsApp');
+  const shoppingCartApp = document.querySelector('.shoppingCartApp');
+  allProductsApp.style.display = 'none';
+  shoppingCartApp.style.display = 'block';
+}
+
+//start app loading all data
+fetchData();
+
+export { getLocalStorage, setLocalStorage };
